5941d98f5eca4216b5ffa5f9c6c8926f8605c9a1
john <john@DESKTOP-84N2Q48>
2022-11-11 15:26:34 -0600

    Add project files.

---
+++ PasswordGenerator/Controllers/PasswordController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using System;
+using System.Globalization;
+
+namespace PasswordGenerator.Controllers
+{
+    public class PasswordController : Controller
+    {
+        public IActionResult Index()
+        {
+            PasswordModel rndPassword = new PasswordModel();
+            return View(rndPassword);
+        }
+
+        [HttpPost]
+        public IActionResult GenerateAlphaNumericPWD(PasswordModel passwordModel)
+        {
+            Random random = new Random();
+            string passwordString = "";
+            int minChars = 33;   
+            int maxChars = 126;
+
+            for (int i = 0; i < passwordModel.passwordLength; i++)
+            {
+                int temp = random.Next(minChars, maxChars);
+                char letter = Convert.ToChar(temp);
+                passwordString = passwordString + letter;
+            }
+            passwordModel.rndPassword = passwordString;
+            return View("Index", passwordModel);
+        }
+        
+    }
+}
---
+++ PasswordGenerator/Models/PasswordModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordGenerator.Models
+{
+    public class PasswordModel
+    {
+        public string rndPassword { get; set; }
+        public int passwordLength { get; set; }
+    }
+}
